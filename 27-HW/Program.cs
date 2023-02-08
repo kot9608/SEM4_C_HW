fff1a3455260824dce74b172f498cdbe383d3ce9
Kirill <Kot9608@mail.ru>
2023-02-08 21:45:43 +0500

    Задача27-upd

--- 27-HW/Program.cs
+++ 27-HW/Program.cs
@@ -8,16 +8,36 @@
 Console.Clear();
 Console.Write("Введите число : ");
 int num = int.Parse(Console.ReadLine());
-int sum = 0;
 if (num<10)
   Console.WriteLine($"Сумма цифр в числе {num} = {num}");
-Console.WriteLine($"Сумма цифр в числе {num} = {CountNumbers(sum)}");
-int CountNumbers(int sum)
+Console.WriteLine($"Сумма цифр в числе {num} = {CountNumbers(num)}");
+int CountNumbers(int NUM)
 {
-    for (int i=1; i<num*10;i++){
-    //int sum = 0;
-    sum = sum+(num%10);
-    num=num/10;   
+int sum=0;
+{
+    for (int i=1; i<NUM*10;i++){
+    
+    sum = sum+(NUM%10);
+    NUM=NUM/10;   
   }
   return sum;
   }
+}
+
+// Console.Clear();
+// Console.Write("Введите целое число: ");
+// int num = int.Parse(Console.ReadLine());
+// Console.WriteLine($"{num} -> {GetSumNumbers(num)}");
+
+// int GetSumNumbers(int numbers)
+// {
+//     int result = 0;
+//     int operand = -1;
+//     while (numbers > 0)
+//     {
+//         operand = numbers % 10;
+//         result += operand;
+//         numbers = numbers / 10;
+//     }
+//     return result;
+// }
