e71ac5b29b9013b655c6b8482121f7c4b2e28733
Kirill <Kot9608@mail.ru>
2023-02-08 22:04:53 +0500

    Задача29-upd

--- 29-HW/Program.cs
+++ 29-HW/Program.cs
@@ -5,18 +5,19 @@
 
 
 
-// Console.Clear();
-// int num = 8;
-// Console.Write("Введите минимальное число для массива: ");
-// int min = int.Parse(Console.ReadLine());
-// Console.Write("Введите максимальное число для массива: ");
-// int max = int.Parse(Console.ReadLine());
-// int[] array = GetBinArray(num);
-// Console.Write($"[{String.Join(", ", array)}]");
-// int[] GetBinArray(int size){
-//     int[] result = new int[size];
-//     for (int i = 0; i < size; i++){
-//         result[i] = new Random().Next(min, max+1);
-//     }
-//     return result;
-// }
+Console.Clear();
+Console.Write("Введите длинну массива: ");
+int s = int.Parse(Console.ReadLine());
+Console.Write("Введите минимальное число для массива: ");
+int min = int.Parse(Console.ReadLine());
+Console.Write("Введите максимальное число для массива: ");
+int max = int.Parse(Console.ReadLine());
+int[] array = GetBinArray(s);
+Console.Write($"[{String.Join(", ", array)}]");
+int[] GetBinArray(int size){
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++){
+        result[i] = new Random().Next(min, max+1);
+    }
+    return result;
+}
